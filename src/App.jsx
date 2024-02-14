0659ab9708bf181e09f68ddc53a9613225114413
Victor Portugal <victorpibrahim@gmail.com>
2024-02-14 11:02:37 -0300

    Correcao na importacao do App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Produtos from './Components/Produtos';
 import Header from './Components/Header';
